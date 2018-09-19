7cd3649410f1ead202eb8b7a409b781cec4ebd0f
Yurick <yurick.hauschild@gmail.com>
2018-09-19 19:14:24 -0300

    ✨  Create base structure

--- src/App.js
+++ src/App.js
@@ -2,16 +2,70 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  )
+export default class App extends React.Component {
+  state = {
+    goal: '',
+    tech: '',
+    limitation: '',
+  }
+
+  randomizeGoal = () => {
+    this.setState({
+      goal: 'a cool goal',
+    })
+  }
+
+  randomizeLimitation = () => {
+    this.setState({
+      limitation: 'a funny limitation',
+    })
+  }
+
+  randomizeTech = () => {
+    this.setState({
+      tech: 'an blazing fast tech',
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <main className="App-intro">
+          <section className="goal">
+            <button
+              type="button"
+              onClick={this.randomizeGoal}
+            >
+              Create Goal
+            </button>
+            {this.state.goal}
+          </section>
+
+          <section className="limitation">
+            <button
+              type="button"
+              onClick={this.randomizeLimitation}
+            >
+              Create Limitation
+            </button>
+            {this.state.limitation}
+          </section>
+
+          <section className="tech">
+            <button
+              type="button"
+              onClick={this.randomizeTech}
+            >
+              Create Tech
+            </button>
+            {this.state.tech}
+          </section>
+        </main>
+      </div>
+    )
+  }
 }
